37e46c10ef25527bf004426097679b996c59fc26
Daniel Y. Zhu <danielyumengzhu@gmail.com>
2025-08-03 01:51:57 -0400

    Add my other publications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
                <Hero craneScale={1.2} wingFlapSpeed={2.8} pathSpeed={11.0} />
 
                {/* Content Sections with white backgrounds */}
-               <Section id="research" title="Research" craneScale={0.8} wingFlapSpeed={2.8} pathSpeed={11.0}>
+               <Section id="research" title="Research" craneScale={0.4} wingFlapSpeed={2.8} pathSpeed={11.0}>
                     <ScrollFadeIn delay={200} duration={800} direction="up">
                          <NewsArticleLayout
                               title="Spateo: Spatiotemporal modeling of molecular holograms"
@@ -36,7 +36,7 @@ export default function Home() {
                               secondaryContentFontSize={14}
                               article1ContentFontSize={14}
                               article2ContentFontSize={14}
-                              fullText="Quantifying spatiotemporal dynamics during embryogenesis is crucial for understanding congenital diseases. We developed Spateo, a 3D spatiotemporal modeling framework, and applied it to a 3D mouse embryogenesis atlas at E9.5 and E11.5, capturing eight million cells. Spateo enables scalable, partial, non-rigid alignment, multi-slice refinement, and mesh correction to create molecular holograms of whole embryos. It introduces digitization methods to uncover multi-level biology from subcellular to whole organ, identifying expression gradients along orthogonal axes of emergent 3D structures, e.g., secondary organizers such as the zona limitans intrathalamica (ZLI). Spateo further jointly models intercellular and intracellular interaction to dissect signaling landscapes in 3D structures. Lastly, Spateo introduces morphometric vector fields of cell migration and integrates spatial differential geometry to unveil molecular programs underlying asymmetrical murine heart organogenesis and others, bridging macroscopic changes with molecular dynamics. Thus, Spateo enables the study of organ ecology at a molecular level in 3D space over time. From both a technological and intellectual perspective, this has been my most complex paper (and certainly the one that has taken the most time and effort!)"
+                              fullText="Quantifying spatiotemporal dynamics during embryogenesis is crucial for understanding congenital diseases. We developed Spateo, a 3D spatiotemporal modeling framework, and applied it to a 3D mouse embryogenesis atlas at E9.5 and E11.5, capturing eight million cells. Spateo enables scalable, partial, non-rigid alignment, multi-slice refinement, and mesh correction to create molecular holograms of whole embryos. It introduces digitization methods to uncover multi-level biology from subcellular to whole organ, identifying expression gradients along orthogonal axes of emergent 3D structures, e.g., secondary organizers such as the zona limitans intrathalamica (ZLI). Spateo further jointly models intercellular and intracellular interaction to dissect signaling landscapes in 3D structures. Lastly, Spateo introduces morphometric vector fields of cell migration and integrates spatial differential geometry to unveil molecular programs underlying asymmetrical murine heart organogenesis and others, bridging macroscopic changes with molecular dynamics. Thus, Spateo enables the study of organ ecology at a molecular level in 3D space over time. From both a technological and intellectual perspective, this has been my most complex paper (and certainly the one that has taken the most time and effort!), and I'm happy to have made something that is useful for many others in the field."
                               article1Title="Related: Mapping cells through space and time with moscot"
                               article1Content="Introduces multi-omics single-cell optimal transport (moscot), a scalable framework for optimal transport in single-cell genomics. This can be used to align mouse cells across space and time."
                               article1PdfPath="/pdf/moscot.pdf"
@@ -45,10 +45,64 @@ export default function Home() {
                               article2PdfPath="/pdf/mouse_atlas.pdf"
                          />
                     </ScrollFadeIn>
+
+                    <ScrollFadeIn delay={200} duration={800} direction="up">
+                         <NewsArticleLayout
+                              title="Defining the determinants of protection against SARS-CoV-2 infection and viral control in a dose-down Ad26. CoV2. S vaccine study in nonhuman primates"
+                              imagePath="/COVID_image.png"
+                              pdfPath="/pdf/COVID_story.pdf"
+                              githubUrl="https://github.com/dzhu8/Ad26-Dose-Down"
+                              date="May 2nd, 2022"
+                              journal="PLoS Biology"
+                              mainContentFontSize={14}
+                              secondaryContentFontSize={14}
+                              article1ContentFontSize={14}
+                              article2ContentFontSize={14}
+                              fullText="Through a dose-down study in non-human primates (macaques), we found Fc effector function to be significantly correlated with mitigation of viral burden. The findings here suggested that vaccination strategies inducing broad functional antibodies would be important for long-term protection. The project that produced this paper was a good introduction to the world of systems biology and a great opportunity to apply statistical principles in practice. It was additionally fulfilling to work so closely to a world event that had a profound impact on my life for a long time (of course referring to the 2020 pandemic)."
+                              article1Title="Related: Compromised Humoral Functional Evolution Tracks with SARS-CoV-2 Mortality"
+                              article1Content="One of the first papers to deeply study the serological correlates of SARS-CoV-2 infection, finding IgG class switching timing to be very important."
+                              article1PdfPath="/pdf/Zohar_et_al_2020.pdf"
+                              article2Title="Related: Selective functional antibody transfer into the breastmilk after SARS-CoV-2 infection"
+                              article2Content="A study of the extent, assessed from serological measurements, to which anti-SARS-CoV-2 immunity is transferred via breastmilk."
+                              article2PdfPath="/pdf/Pullen_et_al_2021.pdf"
+                         />
+                    </ScrollFadeIn>
+
+                    <ScrollFadeIn delay={200} duration={800} direction="up">
+                         <NewsArticleLayout
+                              title="Microtubule growth rates are sensitive to global and local changes in microtubule plus-end density"
+                              imagePath="/MT_density.png"
+                              pdfPath="/pdf/MT_density.pdf"
+                              date="August 3rd, 2020"
+                              journal="Current Biology"
+                              mainContentFontSize={14}
+                              secondaryContentFontSize={14}
+                              article1ContentFontSize={14}
+                              article2ContentFontSize={14}
+                              fullText="We used microfluidics and photopatterning to design self-enclosed hydrogel containers that, when filled with the egg extract from Xenopus laevis, could be used to induce cell-free nucleation of microtubules. From this system, we analyzed the relationship between cytoplasmic volume and microtubule growth dynamics, finding that the density of microtubule plus ends and not cytoplasmic volume dictates microtubule growth rate. This project was my introduction to research in general. I still think it is quite beautiful to realize that these (and similar) complex pictures are constantly painted in our trillions of cells."
+                              article1Title="Related: Changes in cytoplasmic volume are sufficient to drive spindle scaling"
+                              article1Content="This study used a similar experimental setup to investigate the determinants of mitotic spindle sizing."
+                              article1PdfPath="/pdf/spindle_size.pdf"
+                              article2Title="Related: Dynamic instability of microtubule growth"
+                              article2Content="The seminal study by Mitchison & Kirschner that established microtubules as dynamic structures."
+                              article2PdfPath="/pdf/Mitchison_Kirschner_1984.pdf"
+                         />
+                    </ScrollFadeIn>
                </Section>
 
                <Section id="projects" title="Projects" craneScale={0.8} wingFlapSpeed={2.8} pathSpeed={11.0}>
                     <div className="space-y-12">
+                         <ScrollFadeIn delay={100} duration={700} direction="right">
+                              <TextEditWindow
+                                   title="Spateo"
+                                   imagePath="/Spateo_desc.png"
+                                   languages={["python"]}
+                                   content={`A bioinformatics platform. Spateo is a 3D spatiotemporal modeling framework that enables the creation of molecular holograms of whole embryos, allowing for the study of organ ecology at a molecular level in 3D space over time. Spateo enables scalable, 
+                                        partial, non-rigid alignment, multi-slice refinement, and mesh correction to construct 3D holograms from sequential 2D slices. It also introduces digitization methods to uncover multi-level biology from subcellular to whole organ, identifying expression gradients 
+                                        along arbitrary axes. It models intercellular communication to identify genes downstream of cell-cell interaction. Lastly, Spateo introduces "morphometric vector fields" of cell migration and integrates spatial differential geometry to unveil molecular programs 
+                                        underlying this movement.`}
+                              />
+                         </ScrollFadeIn>
                          <ScrollFadeIn delay={100} duration={700} direction="left">
                               <TextEditWindow
                                    title="JSMonitor"
@@ -67,7 +121,7 @@ and an import scanner that checks codebases for import statements that are not y
 (by specifying the path to --config flag) with fallback to a default configuration if none is provided.`}
                               />
                          </ScrollFadeIn>
-                         <ScrollFadeIn delay={100} duration={700} direction="right">
+                         <ScrollFadeIn delay={100} duration={700} direction="left">
                               <CraneAnimatedProjectCard
                                    title="3D Animation Toolbox"
                                    languages={["javascript", "typescript"]}
