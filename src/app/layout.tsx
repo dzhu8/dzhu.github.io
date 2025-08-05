a0bf6f96d328c08c3eae47c344a7a9f30e1eabd9
Daniel Y. Zhu <danielyumengzhu@gmail.com>
2025-08-05 09:07:43 -0400

    Update site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,37 @@ const lora = Lora({
 
 export const metadata: Metadata = {
      title: "Daniel Zhu",
-     description: "Personal website",
+     description: "The personal portfolio of Daniel Zhu, a PhD student in Biological Engineering at MIT, computational biologist and code enthusiast. Learn about my research work, side projects, and hobbies!",
+     keywords: ["Daniel Zhu", "Biological Engineering", "MIT", "Computational Biology", "Software Development"],
+     authors: [
+          {
+               name: "Daniel Zhu",
+               url: "https://danielyzhu.com",
+          },
+     ],
+     creator: "Daniel Zhu",
+     openGraph: {
+          title: "Daniel Zhu",
+          description: "The personal portfolio of Daniel Zhu, a PhD student in Biological Engineering at MIT, computational biologist and code enthusiast. Learn about my research work, side projects, and hobbies!",
+          url: "https://danielyzhu.com",
+          siteName: "Daniel Zhu",
+          images: [
+               {
+                    url: "/daniel_zhu.jpeg",
+                    width: 1200,
+                    height: 630,
+                    alt: "Daniel Zhu",
+               },
+          ],
+          locale: "en_US",
+          type: "website",
+     },
+     twitter: {
+          card: "summary_large_image",
+          title: "Daniel Zhu",
+          description: "The personal portfolio of Daniel Zhu, a PhD student in Biological Engineering at MIT, computational biologist and code enthusiast. Learn about my research work, side projects, and hobbies!",
+          images: ["/daniel_zhu.jpeg"],
+     },
 };
 
 export default function RootLayout({
